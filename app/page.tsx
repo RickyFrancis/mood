778e13d9425cf75b1db462bb6ad9282e747cacd6
Ricky Francis Rozario <rickyfrancisrozario@gmail.com>
2024-08-27 21:45:33 +0200

    Setup Prisma & Auth using Clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="w-screen h-screen bg-black flex justify-center items-center text-yellow-200">
+      <div className="w-full max-w-[600px] mx-auto">
+        <h1 className="text-6xl mb-4">The best Journal app, period.</h1>
+        <p className="text-2xl text-yellow-100 mb-4">
+          This is the best app for tracking the mood throughout your life. All
+          you have to do is be honest.
+        </p>
+        <div>
+          <Link href={'/journal'}>
+            <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
+              Get Started
+            </button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
