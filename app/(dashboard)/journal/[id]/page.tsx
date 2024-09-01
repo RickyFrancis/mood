9c431496beeb18f3cb59b88cc2b0cfc99dadbdcf
Ricky Francis Rozario <rickyfrancisrozario@gmail.com>
2024-09-01 16:21:39 +0200

    Sidebar UI

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -24,8 +24,34 @@ const getEntry = async (id: string) => {
 
 const EntryPage = async ({ params }: EntryPageParams) => {
   const entry = await getEntry(params.id);
+  const analysisData = [
+    { name: 'Subject', value: '' },
+    { name: 'Summary', value: '' },
+    { name: 'Mood', value: '' },
+    { name: 'Negative', value: 'False' },
+  ];
   return (
-    <div className="h-full w-full">{entry && <Editor entry={entry} />}</div>
+    <div className="h-full w-full grid grid-cols-3">
+      <div className="col-span-2">{entry && <Editor entry={entry} />}</div>
+      <div className="border-l border-black/10">
+        <div className="bg-blue-300 px-6 py-10">
+          <h2 className="text-2xl">Analysis</h2>
+        </div>
+        <div>
+          <ul>
+            {analysisData.map((data) => (
+              <li
+                key={data.name}
+                className="flex items-center justify-between px-2 py-4 border-b border-black/10"
+              >
+                <span className="text-lg font-semibold">{data.name}</span>
+                <span>{data.value}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 };
 
